17ed6ca8be74b3827f42ca4aa13c5603d5060d07
j.butajlo <j.butajlo@samsung.com>
2018-11-16 17:28:43 +0100

    Create Profile Use Case

---
+++ domain/src/main/java/com/butajlo/smartprofile/domain/service/ProfileManager.kt
@@ -0,0 +1,10 @@
+package com.butajlo.smartprofile.domain.service
+
+import com.butajlo.smartprofile.domain.entity.ProfileEntity
+
+interface ProfileManager {
+
+    fun createProfile(profile: ProfileEntity): ProfileEntity
+
+    fun updateProfile(profileName: String, profileParams: ProfileEntity.() -> Unit)
+}
