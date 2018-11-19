8dd58e8765c89328a31cd5d4a5660367cf4a0e75
j.butajlo <j.butajlo@samsung.com>
2018-11-19 11:52:42 +0100

    LocationServiceImpl: getLatestLocation, isLocationEnabled

---
+++ app/src/test/java/com/butajlo/smartprofile/permission/PermissionsManagerTest.kt
@@ -0,0 +1,12 @@
+package com.butajlo.smartprofile.permission
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class PermissionsManagerTest {
+
+    @Test
+    fun checkPermissions() {
+    }
+}
