0455a85a6e5e0f235349a9bb9909a6491661bb6d
j.butajlo <j.butajlo@samsung.com>
2018-11-21 09:39:53 +0100

    get location as rx Maybe

--- domain/src/test/java/com/butajlo/smartprofile/domain/usecase/GetLocationTest.kt
+++ domain/src/test/java/com/butajlo/smartprofile/domain/usecase/GetLocationTest.kt
@@ -3,6 +3,7 @@ package com.butajlo.smartprofile.domain.usecase
 import com.butajlo.smartprofile.domain.entity.LocationEntity
 import com.butajlo.smartprofile.domain.service.LocationService
 import com.nhaarman.mockitokotlin2.mock
+import io.reactivex.Maybe
 import org.junit.Test
 
 class GetLocationTest {
@@ -10,13 +11,15 @@ class GetLocationTest {
     val location = LocationEntity(latitude = 0.0, longitude = 0.0)
 
     val locationService = mock<LocationService> {
-        on(it.getLatestLocation()).thenReturn(location)
+        on(it.getLatestLocation()).thenReturn(Maybe.just(location))
     }
 
     val getLocation = GetLocation(locationService)
 
     @Test
     fun getLocation_ShouldReturnLocation() {
-        assert(getLocation() == location)
+        getLocation()
+            .test()
+            .assertResult(location)
     }
 }
